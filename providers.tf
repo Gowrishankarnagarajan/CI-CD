6b82760b08a32f0dedef7d61c82b48252c73f333
Gowrishankarnagarajan <gowrishankarn.2096@gmail.com>
2025-07-03 13:59:02 +0100

    Testing

--- providers.tf
+++ providers.tf
@@ -17,5 +17,16 @@ provider "azurerm" {
 # }
 
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
 
+  required_version = ">= 1.0.0"
 }

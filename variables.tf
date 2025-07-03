9e2e033062d0817c02bdf1ecf1c860f0a6d9330b
Gowrishankarnagarajan <gowrishankarn.2096@gmail.com>
2025-07-03 12:44:28 +0100

    Testing

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-  # variable "subscription_id" {}
-  # variable "tenant_id" {}
-  # variable "client_id" {}
-  # variable "client_secret" {}
+  variable "subscription_id" {}
+  variable "tenant_id" {}
+  variable "client_id" {}
+  variable "client_secret" {}
   variable "prefix" {
     description = "Prefix for resource names"
     default     = "gs"

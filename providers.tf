a8f0a3601b0cc51bed1aa20a30522f5432cb6e36
Gowrishankarnagarajan <gowrishankarn.2096@gmail.com>
2025-07-03 12:29:16 +0100

    Testing

--- providers.tf
+++ providers.tf
@@ -1,10 +1,10 @@
 
 
 provider "azurerm" {
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  # subscription_id = var.subscription_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
+  # tenant_id       = var.tenant_id
   features {}
 }
 # This Terraform configuration sets up an Azure App Service with a resource group, app service plan, and two web apps (frontend and backend).

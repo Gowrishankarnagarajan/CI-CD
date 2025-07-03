2a48b2d414c1e467ca19c80ae37384c8f3901f60
Gowrishankarnagarajan <gowrishankarn.2096@gmail.com>
2025-07-03 12:26:32 +0100

    Testing

--- providers.tf
+++ providers.tf
@@ -8,13 +8,19 @@ provider "azurerm" {
   features {}
 }
 # This Terraform configuration sets up an Azure App Service with a resource group, app service plan, and two web apps (frontend and backend).
-terraform { 
-  cloud { 
+# terraform { 
+#   cloud { 
     
-    organization = "gs-devops" 
+#     organization = "gs-devops" 
 
-    workspaces { 
-      name = "Devops" 
-    } 
-  } 
+#     workspaces { 
+#       name = "Devops" 
+#     } 
+#   } 
+# }
+
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
 }

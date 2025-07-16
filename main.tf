eda7893606019dea5c22e33e2f6b68adf767807c
Gowrishankarnagarajan <gowrishankarn.2096@gmail.com>
2025-07-16 14:47:24 +0100

    Testing

--- main.tf
+++ main.tf
@@ -32,14 +32,14 @@ resource "azurerm_storage_container" "webapp2_sa" {
 }
 
 resource "azurerm_key_vault" "keyvault" {
-  name                        = "${var.prefix}-keyvault-${random_id.kv.hex}"
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "standard"
-  purge_protection_enabled    = true
+  name                     = "${var.prefix}-keyvault-${random_id.kv.hex}"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  sku_name                 = "standard"
+  purge_protection_enabled = true
   #soft_delete_enabled         = true
-  enable_rbac_authorization   = true
+  enable_rbac_authorization = true
 }
 
 resource "azurerm_service_plan" "asp" {
